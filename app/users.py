a0140aacbb1b4b2ba63e029ebbffb4b29245a539
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-04-25 22:39:47 +0300

    Fixed docker and brought some user-library capabilities back

--- app/users.py
+++ app/users.py
@@ -4,7 +4,7 @@ from pydantic import BaseConfig
 from fastapi_users.authentication import JWTAuthentication
 from fastapi_users.db import MongoDBUserDatabase
 from fastapi_users.authentication import CookieAuthentication
-from typing import List
+from typing import List, Optional
 from .models import Bot
 from .db import db
 
@@ -24,7 +24,7 @@ class User(models.BaseUser):
 
 
 class UserCreate(models.BaseUserCreate):
-    pass
+    is_superuser: Optional[str] = None
 
 
 class UserUpdate(User, models.BaseUserUpdate):
@@ -41,6 +41,14 @@ user_db = MongoDBUserDatabase(UserDB, db["users"])
 
 async def on_after_register(user: UserDB, request: Request):
     print(f"User {user.id} has been registered.")
+
+
+def on_after_forgot_password(user: UserDB, token: str, request: Request):
+    print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+
+def after_verification_request(user: UserDB, token: str, request: Request):
+    print(f"Verification requested for user {user.id}. Verification token: {token}")
 
 
 jwt_authentication = JWTAuthentication(
@@ -65,9 +73,22 @@ router.include_router(
 router.include_router(
     fastapi_users.get_auth_router(cookie_authentication), prefix="/auth/cookie", tags=["auth"]
 )
-
 router.include_router(
     fastapi_users.get_register_router(on_after_register), prefix="/auth", tags=["auth"]
+)
+router.include_router(
+    fastapi_users.get_reset_password_router(
+        SECRET, after_forgot_password=on_after_forgot_password
+    ),
+    prefix="/auth",
+    tags=["auth"],
+)
+router.include_router(
+    fastapi_users.get_verify_router(
+        SECRET, after_verification_request=after_verification_request
+    ),
+    prefix="/auth",
+    tags=["auth"],
 )
 
 router.include_router(fastapi_users.get_users_router(), prefix="/users", tags=["users"])
